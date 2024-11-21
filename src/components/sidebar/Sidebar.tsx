0874c11a85897c099f74c99c6329a1b2f22730ba
Nitin Dube <nitindube227@gmail.com>
2024-11-21 12:53:03 +0530

    add

--- src/components/sidebar/Sidebar.tsx
+++ src/components/sidebar/Sidebar.tsx
@@ -27,6 +27,7 @@ const Sidebar = () => {
             ref={sidebarRef}
             id="kt_app_sidebar"
             className={clsx("app-sidebar", config.app?.sidebar?.default?.class)}
+           
           >
             <SidebarLogo sidebarRef={sidebarRef} />
             <SidebarMenu />
