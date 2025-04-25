159123b99939767df3a341ae028b5ed36cd0d603
Nitin Dube <nitindube227@gmail.com>
2025-04-25 16:33:32 +0530

    fix data

--- next.config.mjs
+++ next.config.mjs
@@ -1,14 +1,21 @@
 import createNextIntlPlugin from 'next-intl/plugin';
+import { join } from 'path';
 
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-    
-        images: {
-            domains: ["localhost", "thakkarbappa.vercel.app","thakkarbappa.nrcjewels.com"],
-        },
+  images: {
+    domains: ["localhost", "thakkarbappa.vercel.app", "thakkarbappa.nrcjewels.com"],
+  },
 
-
-
+  // 👇 Add this rewrite
+  async rewrites() {
+    return [
+      {
+        source: '/uploads/:path*',
+        destination: '/uploads/:path*',
+      },
+    ];
+  },
 };
 
 const withNextIntl = createNextIntlPlugin();
@@ -17,5 +24,3 @@ const withNextIntl = createNextIntlPlugin();
 const finalConfig = withNextIntl(nextConfig);
 
 export default finalConfig;
-
-
