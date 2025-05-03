7241963499dc5a690755da07cb9c01237459b419
nitin27 <nitindube227@gmail.com>
2025-05-03 18:11:11 +0530

    add logo

--- next.config.mjs
+++ next.config.mjs
@@ -2,10 +2,10 @@ import createNextIntlPlugin from 'next-intl/plugin';
 
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-    
-        images: {
-            domains: ["localhost", "thakkarbappa.vercel.app","thakkarbappa.nrcjewels.com"],
-        },
+
+    images: {
+        domains: ["localhost", "thakkarbappa.vercel.app", "thakkarbappa.nrcjewels.com", "itdpdeori.weclocks.online"],
+    },
 
 
 
